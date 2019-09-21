3e82812837e010528454e7fa60e5d5704b162400
Daniel Corcoran <danielc.92@outlook.com>
2019-09-21 13:00:08 +1000

    Added todobox style

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="todo-box">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem"
+}
+</script>
+
+<style scoped>
+    .todo-box {
+        background: red;
+        color: white;
+        border: none;
+        border-radius: 2px;
+        margin-bottom: 1rem;
+    }
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,7 +2,9 @@
     <section>
         <h1>Todos</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, quisquam. Ipsa iste sit accusantium laudantium, cupiditate, animi veniam alias velit quae asperiores quidem excepturi atque eveniet consequatur officiis natus aut!</p>
+        <div v-for="(todo, index) in todos" v-bind:key="index">
         
+        </div>
     </section>
 </template>
 
