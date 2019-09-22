742d4a4de4276b40141775384afe35fdf5379bca
daniel corcoran <danielc.92@outlook.com>
2019-09-22 18:08:49 +1000

    Changed todo item with flex

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="todo-box">
-        <h3 class="todo-title">{{ todo.title }}</h3>
-        <p class="todo-para">Ad id laboisi deserunt qui ure et. Cillum nostrud velit commodo consectetur adipisicing irure consequat consectetur ipsum aliquip velit reprehenderit ipsum voluptate. Commodo fugiat id incididunt labore laborum sunt velit amet tempor voluptate.</p>
-        <p>{{ todo.task }}</p>
-        <p class="userid-para">Created by user with id {{ todo.userId }}.</p>
-        <p v-if="!todo.completed" class="todo-reminder"><strong>alert!</strong> You may need to work on this task soon...</p>
-        <p 
+        <div class="space-between-container">
+            <div>
+                <h3 class="todo-title">{{ todo.title }}</h3>
+            </div>
+            <div>
+                <p 
         class='status-tag'
         v-bind:class="[ todo.completed ? 'complete' : 'incomplete']">
         {{ todo.completed ? 'Completed' : 'Incomplete' }}
         </p>
+            </div>
+        </div>
+        
+        <p class="todo-para">Ad id laboisi deserunt qui ure et. Cillum nostrud velit commodo consectetur adipisicing irure consequat consectetur ipsum aliquip velit reprehenderit ipsum voluptate. Commodo fugiat id incididunt labore laborum sunt velit amet tempor voluptate.</p>
+        <p>{{ todo.task }}</p>
+        <p class="userid-para">Created by user with id {{ todo.userId }}.</p>
+        <p v-if="!todo.completed" class="todo-reminder"><strong>alert!</strong> You may need to work on this task soon...</p>
+        
     </div>
 </template>
 
@@ -31,8 +39,13 @@ export default {
         color: rgba(0,0,0,.9);
         border: none;
         border-radius: 0;
-        padding: .4rem 1rem;
+        padding: 1.2rem 2rem;
         border: 1px solid rgba(172, 152, 152, 0.2);
+    }
+
+    .space-between-container {
+        display: flex;
+        justify-content: space-between;
     }
 
     .todo-title {
@@ -50,7 +63,6 @@ export default {
     }
 
     .status-tag {
-        display: inline-block;
         color: white;
         padding: .4rem;
         border-radius: 4px;
@@ -60,16 +72,21 @@ export default {
     }
 
     .incomplete {
-        background: rgba(205, 37, 74, 1);
+        color: rgba(205, 37, 74, 1);
+        border: .1rem solid rgba(205, 37, 74, 1);
     }
     .complete {
-        background: rgba(65, 192, 72, 1);
+        color: rgba(65, 192, 72, .8);
+        border: .1rem solid rgba(65, 192, 72, .5);
     }
 
     .todo-reminder {
+        display: inline-block;
         padding: 1rem;
         background: white;
-        color: var(--danger);
-        border: 1px dotted var(--danger);
+        border-radius: 5px;
+        color: red;
+        border: .1rem solid red;
+        background-color: rgba(255,0,0,.1);
     }
 </style>
